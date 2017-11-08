55fab0b42a108a8f9cac2357a8cbf7fad896f58d
Miko Dimov <mdimov@bu.edu>
2017-11-09 00:51:45 +0100

    initializing app, port, routes, listening

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 'use strict'
 
 // required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 // create express app
+const app = express();
 
 // set app port
+app.set('port', (process.env.PORT || 5000));
 
 // set up bodyparser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-// set up webhook, to be used by FB messenger
+// ROUTES
+
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot")
+});
+
+// Facebook
+
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === "interactmikobot") {
+		res.send(req.query['hub.challenge']);
+	}
+	res.send("Wrong token");
+});
 
 // choose message to send
 
@@ -17,3 +37,6 @@
 // send request
 
 // set up app listening
+app.listen(app.get('port'), function() {
+	console.log("running: port");
+})
