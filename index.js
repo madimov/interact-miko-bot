c2d0048ad8f7ae332cb03bd1468632d58ac9b7b2
Miko Dimov <mdimov@bu.edu>
2017-11-09 00:43:01 +0100

    initializing npm, adding dependencies for express, request, body-parser

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// required modules
+
+// create express app
+
+// set app port
+
+// set up bodyparser
+
+// set up webhook, to be used by FB messenger
+
+// choose message to send
+
+// set up text to be send
+
+// send request
+
+// set up app listening
